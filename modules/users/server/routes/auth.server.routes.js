1fd988561fba56c22cacabe36b16a2e246239826
Douglas Logan <dlogan@cyberninjas.com>
2016-10-16 16:11:57 -0400

    Dynamic Loading of OAuth providers based On if they're in the config.

--- modules/users/server/routes/auth.server.routes.js
+++ modules/users/server/routes/auth.server.routes.js
@@ -19,11 +19,8 @@ module.exports = function (app) {
   app.route('/api/auth/signin').post(users.signin);
   app.route('/api/auth/signout').get(users.signout);
 
-  // Setting the facebook oauth routes
-  app.route('/api/auth/facebook').get(users.oauthCall('facebook', {
-    scope: ['email']
-  }));
-  app.route('/api/auth/facebook/callback').get(users.oauthCallback('facebook'));
+  //Get a list of available strategies
+  app.route('/api/auth/strategies').get(users.getAuthProviders);
 
   // Setting the twitter oauth routes
   app.route('/api/auth/twitter').get(users.oauthCall('twitter'));
